2422955eaf17f55c34978def14889258f77145eb
54padmakshi <pallavi.huilgol@gmail.com>
2025-06-23 16:17:11 +0530

    connected mongodb via .env written in gitignore

--- backend/app.js
+++ backend/app.js
@@ -9,12 +9,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'));
+//connect to mongoDB
+mongoose.connect(process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.error('MongoDB connection error:', err));
 
+    //Api routes
 app.use('/api/sales', salesRoutes);
 
+// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log('Server running on port ${PORT}');
+  console.log(`Server running on port ${PORT}`);
 });
